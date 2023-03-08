ec88186789a416ae421283f00c734cb6dc29c937
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-03-09 01:39:31 +0300

    get request to api /doctor/show-doctors

--- src/features/doctor/DoctorsList.jsx
+++ src/features/doctor/DoctorsList.jsx
@@ -1,14 +1,21 @@
-import {useSelector} from 'react-redux';
-
+import {useSelector, useDispatch} from 'react-redux';
+import {useEffect} from 'react';
+import {getDoctors} from './doctorSlice.js';
 const DoctorsList = () => {
 	
 	const doctors = useSelector((state) => state.doctors);
+	const dispatch = useDispatch();
+
 	const renderDoctors = doctors.map((doctor) => (
 		<div key={doctor.id}>
 			<h1>{doctor.name}</h1>		
 			<h3>{doctor.rank}</h3>
 		</div>
 ))
+
+useEffect(() => {
+	dispatch(getDoctors());
+},[])
 
 return (
 	<div>
--- src/features/doctor/doctorSlice.js
+++ src/features/doctor/doctorSlice.js
@@ -1,4 +1,6 @@
 import {createSlice, nanoid} from '@reduxjs/toolkit';
+import axios from 'axios';
+
 
 const initialState = [
 	{id: nanoid(), name: 'Dr. Hamdi', rank: 'junior'}, 
@@ -9,6 +11,20 @@ const doctorSlice = createSlice({
 
 	name: 'doctors',
 	initialState,
-})
+	reducers: {
+		getDoctors: {
+			reducer (state, action) {
+				axios({
+					method: 'GET',
+					url: '/doctor/show-doctors',
+				}).then((res) => {
+						console.log(res.data);
+					})
+			}
+		}		
+	}
+});
+
+export const {getDoctors} = doctorSlice.actions;
 
 export default doctorSlice.reducer;
