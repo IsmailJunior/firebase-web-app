b6f580a5de7c5146a09614dcabfdc2e3e5f96a0e
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-03-09 00:30:15 +0300

    what is take? everything.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import Layout from "./Layout/Layout";
-import HomePage from "./pages/HomePage";
-import AdminPage from "./pages/AdminPage";
-import AuthPage from "./pages/AuthPage";
-import CreateDoctorPage from "./pages/CreateDoctorPage";
-import { Route, Routes} from "react-router-dom";
 const App = () =>
 {
   return (
-      <Layout>
-        <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin"  element={<AdminPage />} />
-        <Route path="/auth"  element={<AuthPage />} />
-        <Route path="/create-doctor" element={<CreateDoctorPage />} />
-      </Routes>
-      </Layout>
+    <div>
+      <h1>demo</h1>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { UserContextProvider } from "./context/userContext";
-import { DoctorContextProvider } from "./context/doctorContext";
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <DoctorContextProvider>
-    <UserContextProvider>
-      <Router>
-        <App />
-      </Router>
-    </UserContextProvider>
-  </DoctorContextProvider>
+  <App />
 );
