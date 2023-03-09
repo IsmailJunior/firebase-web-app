f2743c4a4d3a8bd8495540977922142e2bd5350d
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-03-09 18:13:27 +0300

    stuff

--- src/features/doctor/AddDoctorForm.jsx
+++ src/features/doctor/AddDoctorForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit';
 import { addDoctor } from './doctorSlice';
 
 const AddDoctorForm = () =>
